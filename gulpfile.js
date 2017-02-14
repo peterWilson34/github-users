cbe3bf4392e721d7efbeba8c6016edf4b5ce7caa
Peter <peter.system.online@gmail.com>
2017-02-14 11:41:22 +0200

    configue gulp & struct the app

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp= require('gulp'),
+    concat=require('gulp-concat'),
+    sass=require('gulp-sass'),
+    watch = require('gulp-watch'),
+    rename=require('gulp-rename');
+
+
+gulp.task('sass',function(){
+  gulp.src(['src/sass/base.scss','src/sass/*.scss']).
+  pipe(concat('app.scss')).
+  pipe(sass()).
+  pipe(gulp.dest('src/css/'))
+})
+gulp.task('styles',function(){
+  gulp.src(['src/css/*.css']).
+  pipe(concat('styles.css')).
+  pipe(gulp.dest('dist/css/'))
+
+})
+gulp.task('scripts',function(){
+  gulp.src(['src/js/app.js','src/js/routes.js','src/js/directives/*.js','src/js/factories/*.js','src/js/controllers/*.js']).
+  pipe(concat('scripts.js')).
+  pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('watch', function() {
+
+  // Watch .scss & .css files
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/css/*.css', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['scripts']);
+
+
+});
+
+gulp.task('default',['sass','styles','scripts','watch']);
---
+++ index.js
@@ -0,0 +1,14 @@
+var http= require('http');
+var express= require('express');
+var app= express();
+
+app.use('/dist',express.static(__dirname+'/dist'))
+app.use('/node_modules',express.static(__dirname+'/node_modules'))
+
+app.get('/',function(req,res) {
+  res.sendFile(__dirname+'/index.html')
+})
+
+app.listen(3000,function(){
+  console.log('listening on port 3000');
+});
